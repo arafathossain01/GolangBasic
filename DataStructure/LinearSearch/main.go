2e1ae9ff45f549e1ebf9017732b77d8c037bdb1a
arafat <arafathossain.cs@gmail.com>
2026-09-25 02:31:39 +0600

    linearSearch

--- DataStructure/LinearSearch/main.go
+++ DataStructure/LinearSearch/main.go
@@ -2,17 +2,55 @@ package main
 
 import "fmt"
 
-func linearSearch(arr []int, find int)int{
+func print(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		fmt.Print(arr[i], "  ")
+	}
+	fmt.Println()
+}
+
+func input(arr []int, target int) int {
+	fmt.Print("Enter the array element: ")
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Scan(&arr[i])
+	}
+	fmt.Print("Enter target value: ")
+	fmt.Scan(&target)
+
+	return target
+}
+
+func linearSearch(arr []int, target int) int {
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == target {
+			return i
+		}
+	}
+	return -1
 
 }
 func main() {
+	var (
+		size   int
+		target int
+	)
+
 	fmt.Print("Enter array size: ")
-	var size int
 	fmt.Scan(&size)
 
 	arr := make([]int, size)
 
-	result = linearSearch(arr[], target)
+	re := input(arr, target)
+	print(arr)
 
+	result := linearSearch(arr, re)
+
+	if result != -1 {
+		fmt.Println("Found ", re, " at position: ", result+1)
+	} else {
+		fmt.Println("Element not found.")
+	}
 
 }
